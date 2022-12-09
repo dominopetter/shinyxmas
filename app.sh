90bb856a72c34955f41ebf8868b48eaee4b22736
Petter Olsson <petter@dominodatalab.com>
2022-12-09 08:10:12 -0800

    Added app.sh
    Pushed from Domino: https://novo-sce.domino-eval.com/workspace/petter/christmas?executionId=63935c0e51b6db79883f5c57

--- app.sh
+++ app.sh
@@ -2,4 +2,4 @@
 # See docs at:
 # https://docs.dominodatalab.com/en/latest/user_guide/e92082/publish-an-r-app-with-shiny/#_publish_an_app
 
-R -e 'shiny::runApp("./R", port=8888, host="0.0.0.0")'
+R -e 'shiny::runApp(port=8888, host="0.0.0.0")'
